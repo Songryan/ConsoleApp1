6409ac696b1a34f0f05db94f7c941a5490e77e09
tjr6255@gmail.com <tjr6255@gmail.com>
2021-12-24 17:33:40 +0900

    211224

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
@@ -6,8 +7,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            
+            //플라스크맨들고 array로 값넘김.
+            //prepare();
+            //넘겨받은값으로 경우의수 
+            //doSort();
+            //경우의수 체크해서 출력
+            //printResult();
 
+             int f_Num1 = 1;              //플라스크고유번호
+             int stackNum1 = 4;        //총량
+            Dictionary<int, int> color_Num1 = new Dictionary<int, int>();             //색번호, 크기
+            
+            //역순으로 push되게.
+            color_Num1.Add(4, 1);
+            color_Num1.Add(3, 1);
+            color_Num1.Add(2, 1);
+            color_Num1.Add(1, 1);
+
+            Stack<Dictionary<int, int>> s = new Stack<Dictionary<int, int>>();
+            
         }
     }
 }
--- set_Flask.cs
+++ set_Flask.cs
@@ -4,22 +4,35 @@ using System.Collections.Generic;
 namespace ConsoleApp1
 {
     public class set_Flask
-	{
-        public int f_Num;
-        public int stackNum;    //총 양
-        Dictionary<int, int> color_Num;
+    {
+        private int f_Num;              //플라스크고유번호
+        private int stackNum;        //총량
+        Dictionary<int, int> color_Num;             //색번호, 크기
         Stack<Dictionary<int, int>> s = new Stack<Dictionary<int, int>>();
 
+        public set_Flask(int f_Num, int stackNum, Dictionary<int, int> color_Num, Stack<Dictionary<int, int>> s)
+        {
+            this.f_Num = f_Num;
+            this.stackNum = stackNum;
+            this.color_Num = color_Num;
+            this.s = s;
+        }
+
+        public int F_Num { get => f_Num; set => f_Num = value; }
+        public int StackNum { get => stackNum; set => stackNum = value; }
+        public Dictionary<int, int> Color_Num { get => color_Num; set => color_Num = value; }
+        public Stack<Dictionary<int, int>> S { get => s; set => s = value; }
+        
     }
+
     public class tot_colorNum
     {
         public int Tot_colors { get; set; }
-        public int start = 1;
         public int Start { get; set; }
         public List<int> col_list;
         public void make_set(List<int> col_list, int Tot_colors)
         {
-
+            int start = 1;
             for (int i = 0; i < Tot_colors; i++)
             {
                 int stNum = start + i;
@@ -27,6 +40,10 @@ namespace ConsoleApp1
             }
 
         }
+    }
 
+    public class prepare
+    {
+        
     }
 }
